de5f628a19a5575530821a4864069e84475756f1
SyedAshar0 <asharjojo0@gmail.com>
2024-09-14 12:37:23 +0500

    Add files via upload

---
+++ Lab hw/question7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   
+    printf("                  ____________    \n");
+    printf("                 /            \\  \n");
+    printf("                /  /\\_/\\       \\ \n");
+    printf("               /  ( . . )  ^    \\ \n");
+    printf("              /    )   (  ((     \\       \n");	
+    printf("\"cat\"        |    ( > < )_))      |   \n");
+    printf("              \\    -----         /     \n");
+    printf("               \\   K24-0582     /       \n");
+    printf("                \\              /      \n");
+    printf("                 \\____________/ \n");
+
+    return 0;
+}
